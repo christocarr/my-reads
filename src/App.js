92af45c8cf55557239d63782da397feef8bdaab8
Chris Carr <christocarr@outlook.comgit config --global user.email christocarr@outlook.comgit config --global user.email christocarr@outlook.com>
2018-07-06 06:55:57 +0100

    Add BookShelves components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React, { Component } from 'react';
-import './App.css';
-import AllBooks from './AllBooks'
+import React, { Component } from 'react'
+import './App.css'
+import BookShelves from './BookShelves'
 
 class App extends Component {
+  
+  // updateQuery = (query) => {
+  //   this.setState({query: query.trim()})
+  // }
+
   render() {
     return (
       <div className="App">
-        <AllBooks />
+        <div>
+          <input type='text' 
+          placeholder='Search books' 
+          // value={this.state.query}
+          // onChange={(event) => this.updateQuery(event.target.value)}
+          />
+        </div>
+          <h1 className='app-header'>My Reads</h1>
+          <BookShelves shelf='Currently Reading' />
+          <BookShelves shelf='Want to Read' />
+          <BookShelves shelf='Read' />
       </div>
     );
   }
